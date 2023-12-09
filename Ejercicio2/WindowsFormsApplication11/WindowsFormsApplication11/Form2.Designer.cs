9eab085fa13a28e9002f564b06ff1e869a36e45f
ag-kaineparker <146886805+ag-kaineparker@users.noreply.github.com>
2023-12-08 22:56:48 -0400

    Add files via upload

---
+++ Ejercicio2/WindowsFormsApplication11/WindowsFormsApplication11/Form2.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApplication11
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPaterno = new System.Windows.Forms.TextBox();
+            this.txtMaterno = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(579, 220);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(167, 343);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(167, 404);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Cambiar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(346, 310);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // txtPaterno
+            // 
+            this.txtPaterno.Location = new System.Drawing.Point(346, 336);
+            this.txtPaterno.Name = "txtPaterno";
+            this.txtPaterno.Size = new System.Drawing.Size(100, 20);
+            this.txtPaterno.TabIndex = 5;
+            // 
+            // txtMaterno
+            // 
+            this.txtMaterno.Location = new System.Drawing.Point(346, 366);
+            this.txtMaterno.Name = "txtMaterno";
+            this.txtMaterno.Size = new System.Drawing.Size(100, 20);
+            this.txtMaterno.TabIndex = 6;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(346, 392);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefono.TabIndex = 7;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 514);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtMaterno);
+            this.Controls.Add(this.txtPaterno);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPaterno;
+        private System.Windows.Forms.TextBox txtMaterno;
+        private System.Windows.Forms.TextBox txtTelefono;
+    }
+}
---
+++ Ejercicio2/WindowsFormsApplication11/WindowsFormsApplication11/Form2.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication11
+{
+    public partial class Form2 : Form
+    {
+        // Declarar el objeto del servicio web a nivel de la clase
+        ServiceReference1.WebService1SoapClient ws = new ServiceReference1.WebService1SoapClient();
+
+        public Form2()
+        {
+            InitializeComponent();
+
+
+                // Asociar el evento SelectionChanged al control DataGridView
+                dataGridView1.SelectionChanged += dataGridView1_SelectionChanged;
+
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // Utilizar el objeto ws para obtener datos de alumnos
+            dataGridView1.DataSource = ws.Alumno().Tables[0];
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Manejar eventos de celda si es necesario
+        }
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            // Manejar el evento de cambio de selección en el DataGridView
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                // Obtener índices de columnas
+                int colNombre = dataGridView1.Columns["Nombre"].Index;
+                int colPaterno = dataGridView1.Columns["Paterno"].Index;
+                int colMaterno = dataGridView1.Columns["Materno"].Index;
+                int colTelefono = dataGridView1.Columns["Telefono"].Index;
+
+                // Obtener datos del alumno seleccionado y mostrarlos en los TextBox
+                txtNombre.Text = dataGridView1.SelectedRows[0].Cells[colNombre].Value.ToString();
+                txtPaterno.Text = dataGridView1.SelectedRows[0].Cells[colPaterno].Value.ToString();
+                txtMaterno.Text = dataGridView1.SelectedRows[0].Cells[colMaterno].Value.ToString();
+                txtTelefono.Text = dataGridView1.SelectedRows[0].Cells[colTelefono].Value.ToString();
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Obtener datos de los TextBox
+            string nuevoNombre = txtNombre.Text;
+            string nuevoPaterno = txtPaterno.Text;
+            string nuevoMaterno = txtMaterno.Text;
+            string nuevoTelefono = txtTelefono.Text;
+
+            // Llamamos al servicio web para agregar un nuevo alumno
+            ws.AgregarAlumno(nuevoNombre, nuevoPaterno, nuevoMaterno, nuevoTelefono);
+
+            // Actualizamos el DataGridView
+            RefreshDataGridView();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Verificamos si hay una fila seleccionada
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                // Obtenemos el ID del alumno seleccionado
+                int idAlumno = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["NewID"].Value);
+
+                // Llamamos al servicio web para eliminar al alumno
+                ws.EliminarAlumno(idAlumno);
+
+                // Actualizamos el DataGridView
+                RefreshDataGridView();
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un alumno para eliminar.");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Verificamos si hay una fila seleccionada
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                // Obtener datos del alumno seleccionado en el DataGridView
+                int idAlumno = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["NewID"].Value);
+                string nuevoNombre = txtNombre.Text;
+                string nuevoPaterno = txtPaterno.Text;
+                string nuevoMaterno = txtMaterno.Text;
+                string nuevoTelefono = txtTelefono.Text;
+
+                // Llamamos al servicio web para cambiar los datos del alumno seleccionado
+                ws.CambiarAlumno(idAlumno, nuevoNombre, nuevoPaterno, nuevoMaterno, nuevoTelefono);
+
+                // Actualizamos el DataGridView
+                RefreshDataGridView();
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un alumno para cambiar.");
+            }
+        }
+       
+
+
+        private void RefreshDataGridView()
+        {
+            // Actualiza el DataGridView con los datos más recientes del servicio web
+            dataGridView1.DataSource = ws.Alumno().Tables[0];
+        }
+    }
+}
